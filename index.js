7e8e724125c84ccc52ce47258aa655c2545d6a26
Alessandro <ale.loguzzo@gmail.com>
2018-09-18 11:39:35 +0200

    dev_11

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var port = process.env.PORT || 8080;
 // create serve and configure it.
 const server = express();
 server.use(bodyParser.json());
-server.post('/getMovies',function (request,response)  {
+server.post('/getWooord',function (request,response)  {
     /*if(request.body.result.parameters['geo-city']) {
         var req = unirest("GET", "https://api.themoviedb.org/3/movie/top_rated");
             req.query({
