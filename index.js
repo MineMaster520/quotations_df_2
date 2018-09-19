0dfa06f5539f29842c18b996277de99c9277f785
Alessandro <ale.loguzzo@gmail.com>
2018-09-19 11:38:24 +0200

    dev_15

--- index.js
+++ index.js
@@ -29,9 +29,9 @@ server.post('/getMovies',function (req,res)  {
 
         requestt.get("http://quote.moveolux.com:88/home/testquote?from=milano&to=roma&day=13/12/2018&time=10:00", (error, response, body) => {
             if(error) {
-                return console.dir(error);
+                //return console.dir(error);
             }
-            console.dir(JSON.parse(body));
+            //console.dir(JSON.parse(body));
             return res.json( {
                 fulfillmentText: JSON.parse(body);
             });
